cb38463a55ad15625b45d290bc261d6eda6dca5e
iOS <www.pusai.net@wuzhiweidemac-mini.local>
2016-09-27 18:14:20 +0800

    搭建基本框架，添加图片

---
+++ WZWVegetableLocal/Resource/Global/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZWVegetableLocal
+//
+//  Created by iOS on 16/9/27.
+//  Copyright © 2016年 wzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
