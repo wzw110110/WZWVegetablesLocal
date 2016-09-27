ed4349fc925def858e37de23fc7b515936b4b653
iOS <www.pusai.net@wuzhiweidemac-mini.local>
2016-09-27 17:40:48 +0800

    Initial Commit

---
+++ WZWVegetableLocal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZWVegetableLocal
+//
+//  Created by iOS on 16/9/27.
+//  Copyright © 2016年 wzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WZWVegetableLocal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WZWVegetableLocal
+//
+//  Created by iOS on 16/9/27.
+//  Copyright © 2016年 wzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
